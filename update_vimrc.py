e4687970107e2ecaaf91561b5961ae17bedce490
nosrac77 <carson.newton@outlook.com>
2018-09-18 19:36:43 -0700

    attempted to run my update_vimrc.py module, failed due to a path specification error. fixing now.

--- update_vimrc.py
+++ update_vimrc.py
@@ -7,10 +7,10 @@ import sys
 
 def run_acp_vimrc_file(branch_name):
     """Function responsible for updating Github .vimrc file."""
-    
+
     original_vimrc = os.environ.get('ORIGINAL_VIMRC', 'Not Set')
     project_dir = os.environ.get('PROJECT_DIR', 'Not Set')
-    
+
     call(['sudo', 'rm', project_dir + '/vimrc'])
     call(['sudo', 'cp', original_vimrc, project_dir])
     call(['sudo', 'mv', project_dir + '/.vimrc', project_dir + '/vimrc'])
