94b01647ad4e5cba2e2cd53dc0538ef19a578cb3
nosrac77 <carson.newton@outlook.com>
2018-09-18 19:31:36 -0700

    modified update_vimrc.py module to copy, rename, and run an acp cycle for updating my .vimrc file. This will automate the process I've been doing by handgit add -- .

--- update_vimrc.py
+++ update_vimrc.py
@@ -5,16 +5,15 @@ import os
 import sys
 
 
-def run_acp_config_files(branch_name):
-    """Function responsible for updating Github configuration files."""
+def run_acp_vimrc_file(branch_name):
+    """Function responsible for updating Github .vimrc file."""
     
-    original_dir = os.environ.get('ORIGINAL_DIR', 'Not Set')
-    path_to_project_dir = os.environ.get('PROJECT_DIR_PATH', 'Not Set')
-    path_to_original_config_dir = os.environ.get('ORIGINAL_DIR_PATH', 'Not Set')
+    original_vimrc = os.environ.get('ORIGINAL_VIMRC', 'Not Set')
+    project_dir = os.environ.get('PROJECT_DIR', 'Not Set')
     
-    call(['sudo', 'rm', '-rf', path_to_project_dir + '/homeassistant_config_backup'])
-    call(['sudo', 'cp', '-r', path_to_original_config_dir, path_to_project_dir])
-    call(['sudo', 'mv', path_to_project_dir + original_dir, path_to_project_dir + '/homeassistant_config_backup'])
+    call(['sudo', 'rm', project_dir + '/vimrc'])
+    call(['sudo', 'cp', original_vimrc, project_dir])
+    call(['sudo', 'mv', project_dir + '/.vimrc', project_dir + '/vimrc'])
     call(['sudo', 'git', 'add', '.'])
     commit_message = input('Please enter a Github commit message: ')
     call(['sudo', 'git', 'commit', '-m', commit_message])
@@ -22,5 +21,5 @@ def run_acp_config_files(branch_name):
 
 
 if __name__ == '__main__':
-    run_acp_config_files(sys.argv[1])
+    run_acp_vimrc_file(sys.argv[1])
 
